e366e2cb5a599faa206726fa2026c835599a2717
Eiman-Ahmed <76091972+Eiman-Ahmed@users.noreply.github.com>
2021-01-06 16:10:42 +0300

    Update pipeline.sh

--- Model_building/Docker_files/pipeline.sh
+++ Model_building/Docker_files/pipeline.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 Rscript --quiet --vanilla /scripts/normalization.R
-Rscript --quiet --vanilla /scripts/szabo_inflammation_signature.R
-Rscript --quiet --vanilla /scripts/icr_signature.R
+#Rscript --quiet --vanilla /scripts/szabo_inflammation_signature.R
+#Rscript --quiet --vanilla /scripts/icr_signature.R
 Rscript --quiet --vanilla /scripts/pathways_signature.R
-Rscript --quiet --vanilla /scripts/MiracleScore.R
+#Rscript --quiet --vanilla /scripts/MiracleScore.R
+Rscript --quiet --vanilla /scripts/Prediction_dummy.R
