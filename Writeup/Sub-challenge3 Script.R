f040d70eb776e7663a6f5686ca25531a67589cf1
Eiman-Ahmed <76091972+Eiman-Ahmed@users.noreply.github.com>
2021-03-16 12:47:28 +0300

    Create Sub-challenge3 Script.R

---
+++ Writeup/Sub-challenge3 Script.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+# Normalizing the raw data 
+suppressMessages(library(data.table))
+suppressMessages(library(yaGST))
+suppressMessages(library(Miracle))
+
+load("/data1/revised_normalized-log2-count.RData")
+load("/data1/ICR_genes.RData")
+
+resMWW <- c()
+for(i in 1:ncol(normalized.log2.count)){
+  resMWW <- c(resMWW,mwwGST(normalized.log2.count[,i],ICR_genes)$nes) 
+}
+
+final_df <- data.frame(patientID = colnames(normalized.log2.count), prediction = -resMWW)
+
+write.table(final_df,file="/output/predictions.csv",quote=F,sep=",",row.names=F,col.names=T)
